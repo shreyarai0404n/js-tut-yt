360f0e13f5e9b05705941d87659b82336c43b09d
shreyarai0404n <162334751+shreyarai0404n@users.noreply.github.com>
2024-03-05 15:01:29 +0000

    constants and variables in js

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId=144553
+let accountEmail="shreya442@gmail.com"
+var accountPassword="12345"
+accountCity="Bangalore"
+console.table([accountId, accountEmail, accountPassword, accountCity])
